5bf4ca3ce9a16d54e6ea082cf3e26f55afbeac46
jhonthe77 <jhonthe7@gmail.com>
2023-10-01 21:32:51 -0500

    header

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 st.set_page_config('Panel De Análisis Empresarial',page_icon='📊',layout='wide' )
 
-st.write('Hecho Por Jhon Kerly Mosquera Este panel se estara atualizando')
+st.subheader('Hecho Por Jhon Kerly Mosquera Este panel se estara atualizando')
 st.markdown("<h1 style='text-align: center;'>Panel De Análisis Empresarial 📊</h1>", unsafe_allow_html=True)
 theme_plotly=None
 df =pd.read_csv('customers.csv')
